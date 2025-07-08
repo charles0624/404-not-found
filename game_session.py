cf1ef7a2f4cdf313696464361f030e88ce575f0e
Samantha Paulson <spaulso3@jh.edu>
2025-07-08 18:58:34 -0400

    Revert "skeletal-createquestion_bugfix"

--- game_session.py
+++
@@ -1,7 +0,0 @@
-
-class GameSession:
-    def __init__(self, users, topics):
-        self.users = users
-        self.topics = topics
-        self.state = "initialized"
-        print(f"[Stub] GameSession created with users {users} and topics {topics}")
--- game_session_manager.py
+++
@@ -1,7 +0,0 @@
-
-class GameSessionManager:
-    def __init__(self, users, topics):
-        print("[Stub] GameSessionManager initialized with users:", users, "and topics:", topics)
-        self.users = users
-        self.topics = topics
-        self.state = "initialized"
